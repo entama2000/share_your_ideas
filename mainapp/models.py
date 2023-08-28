aeebb1093e685aad7891d9538e3c78ad56e66357
entama2000 <entama2000@gmail.com>
2023-08-28 03:24:41 -0400

    create simple models and views

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Ideas(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100, blank=False, default='Anonymous')
+    idea = models.TextField()
+
+    class Meta:
+        ordering = ('created',)
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from mainapp.models import Ideas
+from mainapp.serializers import IdeasSerializer
 
-# Create your views here.
+
+@csrf_exempt
+def idea_list(request):
+    if request.method == 'GET':
+        ideas = Ideas.objects.all()
+        serializer = IdeasSerializer(ideas, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = IdeasSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def idea_detail(request, pk):
+    try:
+        idea = Ideas.objects.get(pk=pk)
+    except Ideas.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = IdeasSerializer(idea)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = IdeasSerializer(idea, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        idea.delete()
+        return HttpResponse(status=204)
